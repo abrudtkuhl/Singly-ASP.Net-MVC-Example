41240c840fa12d57c67ca12ae1e46a760b13b086
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2013-02-12 13:51:19 -0600

    app config for singly

---
+++ SinglyAPIExample/Helpers/AppConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SinglyAPIExample.Helpers
+{
+    public static class AppConfig
+    {
+        /// <summary>
+        /// Gets Singly Client Id from config
+        /// </summary>
+        public static string SinglyClientId
+        {
+            get { return ConfigurationManager.AppSettings["SinglyClientId"]; }
+        }
+
+        /// <summary>
+        /// Gets Singly Secret key from config
+        /// </summary>
+        public static string SinglyClientSecretKey
+        {
+            get { return ConfigurationManager.AppSettings["SinglyClientSecretKey"]; }
+        }
+
+        /// <summary>
+        /// Gets Singly API Base URL from config
+        /// </summary>
+        public static string SinglyApiBaseUrl
+        {
+            get { return ConfigurationManager.AppSettings["SinglyApiBaseUrl"]; }
+        }
+
+        /// <summary>
+        /// Gets Singly Auth Base URL from config
+        /// </summary>
+        public static string SinglyAuthBaseUrl
+        {
+            get { return ConfigurationManager.AppSettings["SinglyAuthBaseUrl"]; }
+        }
+    }
+}
