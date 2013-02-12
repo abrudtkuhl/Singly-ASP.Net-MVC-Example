f6a9de8aadd3c132df84d8b6d520fe51c232806d
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2013-02-12 13:21:30 -0600

    aspnet mvc bundle configs

---
+++ SinglyAPIExample/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SinglyAPIExample.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles()
+        {
+            BundleTable.Bundles.Add(
+                new ScriptBundle("~/bundles/scripts")
+                .Include(
+                "~/Sripts/jquery-1.9.2*",
+                "~/Scripts/knockout*"
+                ));
+        }
+    }
+}
